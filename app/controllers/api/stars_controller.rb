5c281f01696f084db428d1427434301bd6491f0b
immprada <im.mprada@gmail.com>
2022-05-31 18:39:33 -0500

    Add gist_star GET Index endpoint

--- app/controllers/api/stars_controller.rb
+++ app/controllers/api/stars_controller.rb
@@ -3,11 +3,14 @@ module Api
     skip_before_action :verify_authenticity_token
 
     def index
-      byebug
+      @list_of_index = GistStar.all.pluck(:gist_id).uniq
+      get_all_stars_count
+
+      render json: @stars
     end
 
     def create
-      new_gist_star= GistStar.create(gist_id: params[:star][:gist_id])
+      new_gist_star = GistStar.create(gist_id: params[:star][:gist_id])
       
       render json: { result: 'OK', star: new_gist_star}
     end
@@ -16,6 +19,13 @@ module Api
 
     def star_params
       params.require(:star).permit(:gist_id)
+    end
+
+    def get_all_stars_count
+      @stars = {}
+      @list_of_index.each { |index| @stars[index.to_sym] = GistStar.get_stars_count(index) }
+
+      @stars
     end
   end
 end
--- app/models/gist_star.rb
+++ app/models/gist_star.rb
@@ -1,2 +1,9 @@
 class GistStar < ApplicationRecord
+
+  def self.get_stars_count gist_id = nil
+    list=[]
+    list = self.where('gist_id = ?', gist_id) unless !gist_id
+
+    list.size
+  end
 end
