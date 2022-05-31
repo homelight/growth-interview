8a110827eafec2c5afd946d31f50dcefbd66333e
miguel-prada <miguel@modernadvisor.ca>
2022-05-30 20:22:23 -0500

    Implement React

--- app/javascript/packs/Index.tsx
+++ app/javascript/packs/Index.tsx
@@ -1,26 +1,18 @@
 // Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+// import '../styles/global';
 
-const Hello = props => (
-  <div>HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello {props.name}!</div>
+
+const App = () => (
+  <h1>HOLIIIIIIIIIIIIHOLIIIIIIIIIIIIIIIIIIIIIIIIIIII</h1>
 )
-
-Hello.defaultProps = {
-  name: 'DavidDavid'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
+    <App />,
+    document.getElementById('app')
   )
 })
